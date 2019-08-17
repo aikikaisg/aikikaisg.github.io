77c921d537e3f5f4b224554f3980b4c633c8ecb6
julwrites <julian.teh@outlook.com>
2019-08-17 14:09:00 +0800

    Updating with a bare-bones Vue page

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>Aikikai Singapore</h1>
+
+    <p>The Singapore chapter of Aikikai, affiliated to Aikikai Hombu Dojo</p>
+
+    <p>Opened in 1987</p>
+
+
+  </div>
+</template>
---
+++ src/views/Dojo.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>Dojo</h1>
+    <h2>Dojo etiquette</h2>
+    <h2>Classes</h2>
+  </div>
+</template>
