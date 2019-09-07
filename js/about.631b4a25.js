330f57016baa9252294cd1aea6b63399a6bc8c37
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-07 16:06:50 +0000

    Deploy www.aikikai-sg.club to github.com/aikikaisg/aikikaisg.github.io.git:master

--- js/about.631b4a25.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(e,i,t){"use strict";t.r(i);var a=function(){var e=this,i=e.$createElement;e._self._c;return e._m(0)},n=[function(){var e=this,i=e.$createElement,t=e._self._c||i;return t("div",{staticClass:"about"},[t("h1",[e._v("Aikikai Singapore")]),t("p",[e._v("The Singapore chapter of Aikikai, affiliated to Aikikai Hombu Dojo")]),t("p",[e._v("Opened in 1987")])])}],o=t("2877"),r={},u=Object(o["a"])(r,a,n,!1,null,null,null);i["default"]=u.exports}}]);
-//# sourceMappingURL=about.631b4a25.js.map
